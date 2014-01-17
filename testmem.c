705986002a4e630efb85ea3fb6c7ca19a4ebb7ad
dkuhne <daniel.kuhne@s1nn.de>
2014-01-17 13:24:49 +0100

    testmem: check allocation success

--- testmem.c
+++ testmem.c
@@ -13,6 +13,10 @@ int main (int argc, char* argv[]) {
     printf("%s: allocating %d MB and sleeping for %d seconds. \n",argv[0],mem_mb,timeout);
     fflush(stdout);
     p = (char*) malloc(mem_mb*1024*1024);
+    if(NULL == p) {
+        printf("failed to allocatte memory.. exit. \n");
+        return 0;
+    }
     memset(p,0xff,mem_mb*1024*1024);
     sleep(timeout);
     free(p);
